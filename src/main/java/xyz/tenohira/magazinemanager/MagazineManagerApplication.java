306598d89dc0370bbeb0764a54c11ac62085bf96
black099 <a>
2020-08-21 21:25:46 +0900

    ソースファイル等を追加

---
+++ src/main/java/xyz/tenohira/magazinemanager/MagazineManagerApplication.java
@@ -0,0 +1,13 @@
+package xyz.tenohira.magazinemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazineManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/tenohira/MagazineManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.tenohira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazineManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
